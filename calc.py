dd6ea14f9317413e2f28c7b916c042ee874faaa5
Junseong-Yoon <wnstjd4085@gmail.com>
2025-11-05 23:32:33 +0900

    test333

---
+++ calc.py
@@ -0,0 +1,20 @@
+def power(a, b):
+    c = a**b
+    print(f"{c}는 {a}와{b}의 제곱입니다.")
+    return c
+
+def power(a, b):
+    return a * b
+
+def cubic(x):
+    return x ** 3
+
+def divide(a, b):
+    return a / b
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
